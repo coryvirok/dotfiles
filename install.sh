73970d3220544879ffbbe3480dd135a660752030
Cory Virok <coryvirok@gmail.com>
2017-02-20 11:39:06 -0800

    Added ssh-agent/add to bashrc

--- install.sh
+++ install.sh
@@ -9,3 +9,22 @@ ln -svf $PWD/.vimrc ~/.vimrc
 
 [ -f ~/.vim ] && rsync -ahb ~/.tmux.conf ~/.tmux.conf.bak
 ln -svf $PWD/.tmux.conf ~/.tmux.conf
+
+cp ./ssh-find-agent/ssh-find-agent.sh ~/.ssh-find-agent.sh
+
+FIND_AGENT_CMD='. ./.ssh-find-agent.sh'
+if ! grep "$FIND_AGENT_CMD" ~/.bashrc > /dev/null; then
+  ONE_AGENT_TO_RULE_THEM_ALL=$(cat <<-END
+
+# Added by https://github.com/coryvirok/dotfiles/blob/master/install.sh
+# Looks for a running ssh-agent to add SSH keys to or starts up a new
+# one.
+. ./.ssh-find-agent.sh
+set_ssh_agent_socket
+if ! ssh-add -l > /dev/null; then
+  echo 'Adding your SSH keys to SSH agent for the first and last time'
+  ssh-add
+fi
+END)
+  echo "$ONE_AGENT_TO_RULE_THEM_ALL" >> ~/.bashrc
+fi
